c206c4fa35049f2d9037780fdf16ba421ae76b86
pac-cee <pacitekno12@gmail.com>
2025-05-04 13:58:26 +0200

    Update database host from 'localhost' to '127.0.0.1' for improved connectivity

--- OnlineBookstore/config/db.php
+++ OnlineBookstore/config/db.php
@@ -1,6 +1,6 @@
 <?php
 class Database {
-    private $host = "localhost";
+    private $host = "127.0.0.1";
     private $username = "root";
     private $password = "Euqificap12.";
     private $database = "bookapp";
